8b1825b99ee80ac3e5a1053d453adfea5febfdda
kerimliveli <kerimovveli001@gmail.com>
2025-01-30 13:57:15 +0400

    Update

---
+++ ECommerce.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Application.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Controllers
+{
+    public class OrderDetail(IOrderDetailService orderDetailService) : Controller
+    {
+        private readonly IOrderDetailService _orderDetailService = orderDetailService;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECommerce.WebUI/Controllers/SummaryController.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Application.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Controllers
+{
+    public class OrderDetailExtend(IOrderDetailExtendService orderDetailExtendService) : Controller
+    {
+        private readonly IOrderDetailExtendService _orderDetailExtendService = orderDetailExtendService;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
